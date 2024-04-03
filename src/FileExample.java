e8aa091e2b85150704ee1be2ef3c7fa872b95cf4
hoanganh <hoanganhnguyen31122002@gmail.com>
2024-04-03 11:29:52 +0700

    add edit and remove file

--- src/FileExample.java
+++ src/FileExample.java
@@ -4,8 +4,10 @@ public class FileExample {
 
     public static void main(String[] args) throws IOException {
 
-        FileWriter1();
-        FileReader1();
+//        FileWriter1();
+//        FileReader1();
+//          FileEdit();
+//        FileRemove();
     }
 
     static void FileWriter1() throws IOException {
@@ -30,19 +32,50 @@ public class FileExample {
         int ch;
         // check xem file có tồn tại không
         FileReader fr = null;
-        try
-        {
+        try {
             fr = new FileReader("D:\\Code learning\\hoanganhtest.txt");
         }
-        catch (FileNotFoundException fe)
-        {
+        catch (FileNotFoundException fe) {
             System.out.println("File not found");
         }
         // đọc file
-        while ((ch=fr.read())!=-1)
-            System.out.print((char)ch);
+        while ((ch=fr.read())!=-1) {
+            System.out.print((char) ch);
+        }
 
         // đóng trình đọc
         fr.close();
+    }
+
+    static void FileEdit() throws IOException{
+
+        String newData = "da edit file";
+        // check xem file có tồn tại không
+        File file = new File("D:\\Code learning\\hoanganhtest.txt");
+        if(file.exists()){
+            file.delete();
+
+            FileWriter output = new FileWriter("D:\\Code learning\\hoanganhtest.txt");
+            //Ghi string vào file
+            output.write(newData);
+            System.out.println("Write success");
+
+            output.close();
+        }
+        else{
+            System.out.println("file not found");
+        }
+    }
+
+    static void FileRemove() throws IOException{
+
+        File file = new File("D:\\Code learning\\hoanganhtest.txt");
+        if(file.exists()){
+            file.delete();
+            System.out.println("file deleted");
+        }
+        else{
+            System.out.println("file not found");
+        }
     }
 }
